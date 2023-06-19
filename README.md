94ca93169412a5eff242a00e432c5928e978538e
Maximilian Mehner <maximilian.mehner@tuta.io>
2023-06-19 15:19:14 +0200

    minor corrections

--- README.md
+++ README.md
@@ -8,15 +8,16 @@ emacs lisp wrapper for saxon XSLT processors
 
 ## Provided Functions
 This package currently provides the following interactive function
-- **xslt-process-buffer-file** :
-- **xslt-process-buffer-file-tex-compile** : 
+- **xslt-process-buffer-file** : apply stylesheet and find buffer in another window
+- **xslt-process-buffer-file-tex-compile** : apply stylesheet, compile resulting LaTeX-file and show that in PDF viewer 
 
 ## Installation
 1. Add `xslt-wrapper-for-saxon.el` to a directory included in the list `load-path` or modify this list to include the directory with
    `(add-to-list 'load-path "/PATH/TO/el-xslt-wrapper-for-saxon/")` in your init file,
-2. add `(require 'el-xslt-wrapper-for-saxon)` to your init-file,
-3. set the global variable `xslt-wrap-saxonpath` with `(setq xslt-wrap-saxonpath "/PATH/TO/saxonXhe.jar")`,
-4. (optional) set the global variable `xslt-wrap-texcommand` for instance with `(setq xslt-wrap-texcommand "latexmk -pdflatex")`, the default is pdflatex.
+2. add `(require 'xslt-wrapper-for-saxon)` to your init-file,
+3. set the global variable `xslt-wrapper-saxonpath` with `(setq xslt-wrapper-saxonpath "/PATH/TO/saxonXhe.jar")`,
+4. (optional) set the global variable `xslt-wrapper-texcommand` for instance with `(setq xslt-wrapper-texcommand "latexmk -pdflatex")`, the default is pdflatex,
+5. (optional) set the global variable `xslt-wrapper-pdfviewer` for instance with `(setq xslt-wrapper-pdfviewer "mupdf")`, if unset defaulting to Emacs DocView mode.
 
 ## Usage
 1. Evaluate one the above functions with `M-x xslt-process-buffer` or `M-x xslt-process-buffer-tex-compile`,
--- xslt-wrapper-for-saxon.el
+++ xslt-wrapper-for-saxon.el
@@ -1,24 +1,21 @@
-(provide 'el-xslt-wrapper-for-saxon)
+(provide 'xslt-wrapper-for-saxon)
 
-(defvar xslt-wrap-saxonpath ""
+(defvar xslt-wrapper-saxonpath ""
   "The path of the saxon xslt-processor jar-file for xslt-wrapper")
 
-(defvar xslt-wrap-texcommand "pdflatex"
+(defvar xslt-wrapper-texcommand "pdflatex"
   "LaTeX compile command for xslt-wrapper, defaults to pdflatex")
+
+(defvar xslt-wrapper-pdfviewer ""
+  "pdfviewer-command, if unset default to emacs")
 
 (defun  xslt-process-buffer-file ( &optional proc xsl ext)
   (interactive)
-  (unless proc (setq proc xslt-wrap-saxonpath))
-  (unless xsl (setq xsl (xslt-get-encoded-stylesheet)))
-  (unless xsl (setq xsl (read-string "Path of XSL stylesheet: ")))
-  
   (unless ext  (setq ext (read-string "Extension of transformation target: ")))
   
-  (xslt-check-files proc)
-  
   (let ((outfile (concat (substring (buffer-file-name) 0 -3) ext)))
-    (shell-command (concat "java -jar " proc " -s:" (buffer-file-name) " -xsl:" xsl  " -o:" outfile ) "*xslt-out*" "*xslt-err*")
-    
+    (xslt-process proc xsl (buffer-file-name) outfile)
+
     (find-file-other-window outfile)
     (revert-buffer outfile)
     )
@@ -26,27 +23,36 @@
 
 (defun  xslt-process-buffer-file-tex-compile ( &optional proc xsl comp )
   (interactive)
-  (unless proc (setq proc xslt-wrap-saxonpath))
-  (unless xsl (setq xsl (xslt-get-encoded-stylesheet)))
-  (unless xsl (setq xsl (read-string "Path of XSL stylesheet: ")))
-  
-  (unless comp (setq comp xslt-wrap-texcommand))
-
-  (xslt-check-files proc)
+  (unless comp (setq comp xslt-wrapper-texcommand))
+  (unless comp (setq comp (read-string "LaTeX compile command: ")))
 
   (let ((outfile (concat (substring (buffer-file-name) 0 -3) "tex"))
 	(pdffile (concat (substring (buffer-file-name) 0 -3) "pdf")))
-    (shell-command (concat "java -jar " proc " -s:" (buffer-file-name) " -xsl:" xsl " -o:" outfile ) "*xslt-out*" "*xslt-err*")
-    (shell-command (concat comp " " outfile) "*latex-out*" "*latex-err*")
+
+    (xslt-process proc xsl (buffer-file-name) outfile)
     
-    (find-file-other-window pdffile)
-    (revert-buffer pdffile)
+    (shell-command (concat comp " " outfile) "*latex-out*" "*latex-out*")
+
+    (if (not (eq xslt-wrapper-pdfviewer ""))
+	(shell-command (concat xslt-wrapper-pdfviewer " " pdffile))
+      (find-file-other-window pdffile)
+      (revert-buffer pdffile))
     )
   )
+
+(defun xslt-process (proc xsl basexml outfile)
+  (unless proc (setq proc xslt-wrapper-saxonpath))
+  (unless xsl (setq xsl (xslt-get-encoded-stylesheet)))
+  (unless xsl (setq xsl (read-string "Path of XSL stylesheet: ")))
+
+  (xslt-check-files basexml proc)
   
-(defun xslt-check-files ( proc )
-  (unless (file-exists-p (buffer-file-name)) (error "buffer file does not exist"))
-  (unless (file-exists-p proc) (error "processor not found, please set xslt-wrap-saxonpath correctly."))
+  (shell-command (concat "java -jar " proc " -s:" basexml " -xsl:" xsl  " -o:" outfile ) "*xslt-out*" "*xslt-out*")
+  )
+
+(defun xslt-check-files ( basexml proc )
+  (unless (file-exists-p basexml) (error "buffer file does not exist"))
+  (unless (file-exists-p proc) (error "processor not found, please set xslt-wrapper-saxonpath correctly."))
   )
 
 (defun xslt-get-encoded-stylesheet ()
